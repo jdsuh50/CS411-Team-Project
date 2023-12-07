52e2f273c59fc904be792fc646f7388f654a83f8
Carson Mo <carsonmo@bu.edu>
2023-12-06 19:56:20 -0500

    Images Load after Search
    Images of recipes (only 2 to prevent API Key from running out too fast) load after you search

--- HTML Files/frontEnd.js
+++ HTML Files/frontEnd.js
@@ -22,9 +22,47 @@ function callAPI() {
     let apiKey = ;
     callStr = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + apiKey;
     callStr += '&query=' + str;
+    callStr += '&number=2';
     console.log(callStr);
    
+
+    let object; 
+
     fetch(callStr)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+        console.log(data); // Log the data if needed
+  
+        // Store the JSON object in the variable 'object'
+        object = data;
+  
+        // Now you can use the 'object' variable here or pass it to other functions
+        console.log(object.results[0].image);
+        loadImages(object)
+    })
+
+    
+}
+
+function loadImages(object) {
+
+    console.log(object);
+    let l = object.results.length
+
+    let imageTable = document.getElementById("recipeImages");
+    let tableHTML = "<table>\n\t<tr>\n\t\t";
+    let imageURL = "";
+
+    for (let i=0; i<l; i++) {
+        imageURL = object.results[i].image;
+        tableHTML += "<td><img src=\"";
+        tableHTML += imageURL;
+        tableHTML += "\" /></td>"
+    }
+
+    // End the table
+    tableHTML += "</tr>\n </table>";
+    // Add the table HTML
+    imageTable.innerHTML = tableHTML;
+
 }
