8f307bbd640fdb805a37fcad93174a96f0605c77
Carson Mo <carsonmo@bu.edu>
2023-12-09 23:43:18 -0500

    Email var
    Stores email of logged-in user in variable called "email"

--- foodpal/src/components/logInPage.js
+++ foodpal/src/components/logInPage.js
@@ -21,6 +21,8 @@ function LogInPage() {
     console.log("Encoded JWT ID Token: " + response.credential);
     let userObject = jwtDecode(response.credential);
     console.log(userObject);
+    let email = userObject.email;
+    console.log(email);
   }
 
   useEffect(() => {
