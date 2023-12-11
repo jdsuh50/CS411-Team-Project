987102f5aebbebb91dae254242dcaae2e0128b02
Carson Mo <carsonmo@bu.edu>
2023-12-10 21:52:30 -0500

    Added Intolerances + Email to Profile Page
    Can now choose Intolerances (via checkboxes) and enter your Address
    
    Both are logged in console as strings

--- foodpal/src/components/YourProfilePage.js
+++ foodpal/src/components/YourProfilePage.js
@@ -71,4 +71,136 @@ function DietCheckboxList() {
   );
 }
 
-export default DietCheckboxList;
+function IntoleranceCheckboxList() {
+  // State to manage intolerance checkbox values
+  const [intolerances, setIntolerances] = useState([
+    { name: 'dairy', label: 'Dairy', checked: false },
+    { name: 'egg', label: 'Egg', checked: false },
+    { name: 'gluten', label: 'Gluten', checked: false },
+    { name: 'grain', label: 'Grain', checked: false },
+    { name: 'peanut', label: 'Peanut', checked: false },
+    { name: 'seafood', label: 'Seafood', checked: false },
+    { name: 'sesame', label: 'Sesame', checked: false },
+    { name: 'shellfish', label: 'Shellfish', checked: false },
+    { name: 'soy', label: 'Soy', checked: false },
+    { name: 'sulfite', label: 'Sulfite', checked: false },
+    { name: 'treeNut', label: 'Tree Nut', checked: false },
+    { name: 'wheat', label: 'Wheat', checked: false },
+  ]);
+
+  // State to manage selected diets as a string
+  const [selectedIntolerancesString, setSelectedIntolerancesString] = useState('');
+
+  // Event handler for diet checkbox changes
+  const handleIntoleranceCheckboxChange = (index) => {
+    setIntolerances((prevIntolerances) => {
+      const newIntolerances = [...prevIntolerances];
+      newIntolerances[index].checked = !newIntolerances[index].checked;
+      return newIntolerances;
+    });
+  };
+
+  // Update selectedDietsString based on the selected diets
+  useEffect(() => {
+    const selectedIntolerances = intolerances
+      .filter((intolerance) => intolerance.checked)
+      .map((intolerance) => intolerance.label)
+      .join(', ');
+
+    setSelectedIntolerancesString(selectedIntolerances);
+  }, [intolerances]);
+
+  // Log selected diets to the console
+  useEffect(() => {
+    console.log('Selected Intolerances:', selectedIntolerancesString);
+  }, [selectedIntolerancesString]);
+
+  return (
+    <div>
+      <h2>Select Your Intolerance(s)</h2>
+      <form>
+        {intolerances.map((intolerance, index) => (
+          <div key={index}>
+            <label>
+              {intolerance.label}:
+              <input
+                type="checkbox"
+                name={intolerance.name}
+                checked={intolerance.checked}
+                onChange={() => handleIntoleranceCheckboxChange(index)}
+              />
+            </label>
+          </div>
+        ))}
+      </form>
+
+      <div>
+        <h3>Selected Intolerances:</h3>
+        <ul>
+          {intolerances.map((intolerance) => (
+            <li key={intolerance.name}>
+              {intolerance.name}: {intolerance.checked ? 'Selected' : 'Not Selected'}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function AddressForm() {
+  // State to manage form data
+  const [addressData, setAddressData] = useState({
+    address: '',
+  });
+
+  // Event handler for input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setAddressData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // Event handler for "Save Address" button click
+  const handleSaveAddress = () => {
+    // Log the address to the console
+    console.log('Address:', addressData.address);
+  };
+
+  return (
+    <div>
+      <h2>Enter your Address:</h2>
+      <form>
+        <label>
+          <input
+            type="text"
+            name="address"
+            value={addressData.address}
+            onChange={handleChange}
+            placeholder="Enter your address"
+          />
+        </label>
+
+        <br />
+
+        <button type="button" onClick={handleSaveAddress}>
+          Save Address
+        </button>
+      </form>
+    </div>
+  );
+}
+
+function YourProfilePage() {
+  return (
+    <div className="YourProfilePage">
+      <DietCheckboxList />
+      <IntoleranceCheckboxList />
+      <AddressForm />
+    </div>
+  )
+}
+
+export default YourProfilePage;
